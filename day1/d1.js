bfa8b195c1d0ca575c1a2e3d939367de940ccb75
Will Kee <william.w.kee@gmail.com>
2023-12-01 17:00:26 -0700

    day 1 part 1 and 2 complete

--- day1/d1.js
+++ day1/d1.js
@@ -1,8 +1,10 @@
 const parseTextFileIntoArray = require("../utils/parseText");
-// const parsedArray = parseTextFileIntoArray("./example.txt");
-const parsedArray = parseTextFileIntoArray("./input.txt");
+const parsedArrayEx1 = parseTextFileIntoArray("./example1.txt");
+const parsedArrayEx2 = parseTextFileIntoArray("./example2.txt");
+const inputArray = parseTextFileIntoArray("./input.txt");
 
 const isNum = (val) => !isNaN(Number(val));
+const sumArray = (array) => array.reduce((acc, num) => acc + num, 0);
 
 function sumOfCalibrationValues(array) {
 	const arrayOfNums = [];
@@ -31,12 +33,55 @@ function sumOfCalibrationValues(array) {
 		arrayOfNums.push(combined);
 	}
 
-	let answer = 0;
-	for (const num of arrayOfNums) {
-		answer += num;
-	}
-
-	return answer;
+	return sumArray(arrayOfNums);
 }
 
-console.log(sumOfCalibrationValues(parsedArray));
+function sumOfCalibrationValuesP2(array) {
+	const map = {
+		one: 1,
+		two: 2,
+		three: 3,
+		four: 4,
+		five: 5,
+		six: 6,
+		seven: 7,
+		eight: 8,
+		nine: 9,
+	};
+
+	const allNums = [];
+
+	for (const row of array) {
+		const rowNums = [];
+
+		for (const num of Object.keys(map)) {
+			const matches = [...row.matchAll(num)];
+			if (matches.length) {
+				matches.forEach((match) => {
+					const numFormat = map[num];
+					rowNums.push({ num: numFormat, idx: match.index });
+				});
+			}
+		}
+
+		for (let i = 0; i < row.length; i++) {
+			const num = row[i];
+			if (isNum(num)) {
+				rowNums.push({ num: Number(num), idx: i });
+			}
+		}
+
+		rowNums.sort((a, b) => a.idx - b.idx);
+		const firstNum = rowNums[0].num;
+		const lastNum = rowNums[rowNums.length - 1].num;
+		const combined = Number(firstNum.toString() + lastNum.toString());
+		allNums.push(combined);
+	}
+	console.log(allNums);
+	return sumArray(allNums);
+}
+
+// console.log(sumOfCalibrationValues(parsedArrayEx1));
+// console.log(sumOfCalibrationValuesP2(parsedArrayEx2));
+// console.log(sumOfCalibrationValues(inputArray));
+console.log(sumOfCalibrationValuesP2(inputArray));
