5df47057f898fa376af086db8412313f1dc85abe
Will Kee <william.w.kee@gmail.com>
2023-12-21 16:00:42 -0700

    day 6 part 2 complete

--- day06/d6.js
+++ day06/d6.js
@@ -34,36 +34,21 @@ function Day6Part1(arr) {
 }
 
 function Day6Part2(arr) {
-	const times = arr[0].split(/\D/).filter((el) => el !== "");
-	const dist = arr[1].split(/\D/).filter((el) => el !== "");
-	for (let i = 0; i < times.length; i++) times[i] = Number(times[i]);
-	for (let i = 0; i < dist.length; i++) dist[i] = Number(dist[i]);
+	const time = Number(arr[0].replace(/\D/g, ""));
+	const record = Number(arr[1].replace(/\D/g, ""));
 
-	const winNums = [];
-
-	for (let i = 0; i < times.length; i++) {
-		// Each iteration of this outer loop is a separate game.
-		const record = dist[i];
-		const totalTime = times[i];
-		let winCount = 0;
-		// we're skipping t of 0 and total time because winning is impossible.
-		for (let t = 1; t < totalTime; t++) {
-			// For each game we're checking each time t to see the distance
-			const moveTime = totalTime - t;
-			const distance = moveTime * t;
-
-			if (distance > record) winCount++;
-		}
-		winNums.push(winCount);
+	let winCount = 0;
+	// we're skipping t of 0 and total time because winning is impossible.
+	for (let t = 1; t < time; t++) {
+		// For each game we're checking each time t to see the distance
+		const moveTime = time - t;
+		const distance = moveTime * t;
+		if (distance > record) winCount++;
 	}
-
-	let product = 1;
-	for (const num of winNums) product *= num;
-
-	return product;
+	return winCount;
 }
 
 // console.log(Day6Part1(parsedArrayEx1));
 // console.log(Day6Part1(inputArray));
 console.log(Day6Part2(parsedArrayEx1));
-// console.log(Day6Part2(inputArray));
+console.log(Day6Part2(inputArray));
