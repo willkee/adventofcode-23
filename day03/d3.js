0bf31f8cc7d6484d48edb5ff2b5d09645b78c80a
Will Kee <william.w.kee@gmail.com>
2023-12-20 22:17:19 -0700

    renamed folders

---
+++ day03/d3.js
@@ -0,0 +1,3 @@
+const parseTextFileIntoArray = require("../utils/parseText");
+const parsedArrayEx1 = parseTextFileIntoArray("./example1.txt");
+const inputArray = parseTextFileIntoArray("./input.txt");
