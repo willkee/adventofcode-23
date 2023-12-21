d8187a6f2605a4fb05314033dd2b89b6308dab9d
Will Kee <william.w.kee@gmail.com>
2023-12-21 14:36:00 -0700

    day 5 part 2 complete

--- day05/d5.js
+++ day05/d5.js
@@ -97,5 +97,82 @@ function P1(arr) {
 	return ans;
 }
 
-console.log(P1(parsedExample));
-console.log(P1(parsedInput));
+function P2(arr) {
+	const input = arr.split("\n\n");
+	const seeds = input[0].split(": ")[1].split(" ");
+	const seedToSoil = input[1].split(":\n")[1].split("\n");
+	const soilToFert = input[2].split(":\n")[1].split("\n");
+	const fertToWater = input[3].split(":\n")[1].split("\n");
+	const waterToLight = input[4].split(":\n")[1].split("\n");
+	const lightToTemp = input[5].split(":\n")[1].split("\n");
+	const tempToHum = input[6].split(":\n")[1].split("\n");
+	const humToLoc = input[7].split(":\n")[1].split("\n");
+
+	for (const i in seedToSoil) seedToSoil[i] = seedToSoil[i].split(" ");
+	for (const i in soilToFert) soilToFert[i] = soilToFert[i].split(" ");
+	for (const i in fertToWater) fertToWater[i] = fertToWater[i].split(" ");
+	for (const i in waterToLight) waterToLight[i] = waterToLight[i].split(" ");
+	for (const i in lightToTemp) lightToTemp[i] = lightToTemp[i].split(" ");
+	for (const i in tempToHum) tempToHum[i] = tempToHum[i].split(" ");
+	for (const i in humToLoc) humToLoc[i] = humToLoc[i].split(" ");
+
+	for (let i = 0; i < seeds.length; i++) seeds[i] = Number(seeds[i]);
+
+	convertToNum(seedToSoil);
+	convertToNum(soilToFert);
+	convertToNum(fertToWater);
+	convertToNum(waterToLight);
+	convertToNum(lightToTemp);
+	convertToNum(tempToHum);
+	convertToNum(humToLoc);
+
+	// idx 0: destination range start
+	// idx 1: source range start
+	// idx 2: range (including start, not including end)
+
+	const step = (seed, conversion) => {
+		for (let i = 0; i < conversion.length; i++) {
+			const [minDest, minSource, range] = conversion[i];
+			if (seed >= minSource) {
+				const maxSource = minSource + range - 1;
+
+				// Any source numbers that aren't mapped correspond to the same destination number.
+				if (seed > maxSource) continue;
+
+				// seed is in range
+				const offset = minDest - minSource;
+				return seed + offset;
+			}
+		}
+		return seed;
+	};
+
+	let ans = Infinity;
+
+	for (let i = 0; i < seeds.length; i += 2) {
+		const seed = seeds[i];
+		const currentRange = seeds[i + 1];
+
+		for (let j = 0; j < currentRange; j++) {
+			const currentSeed = seed + j;
+			const soil = step(currentSeed, seedToSoil);
+			const fertilizer = step(soil, soilToFert);
+			const water = step(fertilizer, fertToWater);
+			const light = step(water, waterToLight);
+			const temp = step(light, lightToTemp);
+			const humidity = step(temp, tempToHum);
+			const location = step(humidity, humToLoc);
+
+			ans = Math.min(ans, location);
+		}
+	}
+
+	return ans;
+}
+
+// console.log(P1(parsedExample));
+// console.log(P1(parsedInput));
+// console.log(P2(parsedExample));
+
+// EXTREME BRUTE FORCE: UNCOMMENT AT YOUR OWN RISK!!
+// console.log(P2(parsedInput));
