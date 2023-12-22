3f86f2f8d3481fefb5864d6a1e3a05a5da415f2b
Will Kee <william.w.kee@gmail.com>
2023-12-22 14:32:42 -0700

    day 8 part 2 complete

--- day08/d8.js
+++ day08/d8.js
@@ -4,6 +4,7 @@
 // const inputArray = parseTextFileIntoArray("./input.txt");
 
 const fs = require("fs");
+const { GCD } = require("../utils/library");
 
 function parseTextFile(inputPath) {
 	let file;
@@ -18,6 +19,7 @@ function parseTextFile(inputPath) {
 
 const parsedExample1 = parseTextFile("./example1.txt");
 const parsedExample2 = parseTextFile("./example2.txt");
+const parsedExample3 = parseTextFile("./example3.txt");
 const parsedInput = parseTextFile("./input.txt");
 
 function processInput(arr) {
@@ -55,6 +57,63 @@ function D8P1(arr) {
 	return steps;
 }
 
-console.log(D8P1(parsedExample1));
-console.log(D8P1(parsedExample2));
-console.log(D8P1(parsedInput));
+function D8P2(arr) {
+	const { dirs, map } = processInput(arr);
+
+	const current = Object.keys(map).filter((key) => key.endsWith("A"));
+
+	// let done = false;
+	// while (!done) {
+	// 	done = true;
+
+	// 	if (pointer >= dirs.length) pointer = 0;
+	// 	const currentDir = dirs[pointer];
+
+	// 	for (let i = 0; i < current.length; i++) {
+	// 		const nextDirs = map[current[i]];
+	// 		const nextEl = nextDirs[currentDir];
+	// 		current[i] = nextEl;
+	// 	}
+
+	// 	steps++;
+	// 	pointer++;
+
+	// 	for (const el of current) {
+	// 		if (el[el.length - 1] !== "Z") {
+	// 			done = false;
+	// 			break;
+	// 		}
+	// 	}
+	// }
+
+	// Worked on test case but timed out on main input.
+
+	const nums = [];
+
+	for (let i = 0; i < current.length; i++) {
+		let pointer = 0;
+		let steps = 0;
+
+		let node = current[i];
+		while (!node.endsWith("Z")) {
+			if (pointer >= dirs.length) pointer = 0;
+
+			const nextDir = dirs[pointer];
+			const nextNode = map[node][nextDir];
+			node = nextNode;
+
+			steps++;
+			pointer++;
+		}
+		nums.push(steps);
+	}
+
+	const LCM = (a, b) => (a * b) / GCD(a, b);
+	return nums.reduce((acc, cur) => LCM(acc, cur), 1);
+}
+
+// console.log(D8P1(parsedExample1));
+// console.log(D8P1(parsedExample2));
+// console.log(D8P1(parsedInput));
+console.log(D8P2(parsedExample3));
+console.log(D8P2(parsedInput));
--- utils/library.js
+++ utils/library.js
@@ -1,3 +1,15 @@
 const sumArray = (array) => array.reduce((acc, num) => acc + num, 0);
 
-module.exports = { sumArray };
+const GCD = (a, b) => {
+	a = Math.abs(a);
+	b = Math.abs(b);
+
+	if (b > a) [a, b] = [b, a];
+	while (true) {
+		if (b === 0) return a;
+		a %= b;
+		if (a === 0) return b;
+		b %= a;
+	}
+};
+module.exports = { sumArray, GCD };
