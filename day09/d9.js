ce8c43cf609a9db47790023d0d7cf727a26cd8b7
Will Kee <william.w.kee@gmail.com>
2023-12-22 11:44:43 -0700

    day 8 input

---
+++ day09/d9.js
@@ -0,0 +1,4 @@
+const parseTextFileIntoArray = require("../utils/parseText");
+const parsedArrayEx1 = parseTextFileIntoArray("./example1.txt");
+// const parsedArrayEx2 = parseTextFileIntoArray("./example2.txt");
+const inputArray = parseTextFileIntoArray("./input.txt");
